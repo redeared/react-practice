de5d0617967e096012f45341b7bc5a114dd3be92
Andrey Misaylov <misailovaa@gmail.com>
2018-04-29 19:50:38 +0300

    Replace " to '. Remove not used input params in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,40 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = function (env, options) {
-
-    const isProduction = process.env.NODE_ENV ? process.env.NODE_ENV.trim()  === "production": false;
+module.exports = () => {
+    const isProduction = process.env.NODE_ENV ? process.env.NODE_ENV.trim() === 'production' : false;
 
     const config = {
-        context: path.join(__dirname, "src"),
-        entry: "./index",
-        mode: isProduction ? "production" : "development",
-        devtool: isProduction ? "none" : "source-map",
+        context: path.join(__dirname, 'src'),
+        entry: './index',
+        mode: isProduction ? 'production' : 'development',
+        devtool: isProduction ? 'none' : 'source-map',
 
         resolve: {
-            extensions: [".js", ".jsx"]
+            extensions: ['.js', '.jsx'],
         },
 
         module: {
             rules: [
                 {
                     test: /\.jsx?$/,
-                    loader: "babel-loader",
-                    exclude: /node_modules/
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
                 },
-            ]
+            ],
         },
 
         plugins: [
             new HtmlWebpackPlugin({
-                title: "App",
+                title: 'App',
                 hash: true,
-                template: "./index.html"
-            })
+                template: './index.html',
+            }),
         ],
 
         devServer: {
-            contentBase: "./dist"
-        }
+            contentBase: './dist',
+        },
     };
 
     return config;
