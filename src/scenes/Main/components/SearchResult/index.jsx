14b5f5464d9f779670a8a882e702d4e89efbaf84
Andrey Misaylov <misailovaa@gmail.com>
2018-05-04 23:08:06 +0300

    refactoring SearchResult

--- src/scenes/Main/components/SearchResult/index.jsx
+++ src/scenes/Main/components/SearchResult/index.jsx
@@ -21,29 +21,27 @@ class SearchResult extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            onItemClick: () => { },
-            ...props,
+            sortByRating: true,
+            sortByReleaseDate: false,
         };
-        this.state.previousState = props.items;
-    }
-    componentDidMount() {
-        // default sort
-        this.changeItemsOrder({ sortByRating: true, sortByReleaseDate: false });
     }
     getItems = () => (Array.isArray(this.props.items) ? this.props.items : []);
 
-    changeItemsOrder = ({ sortByRating = false, sortByReleaseDate = false }) => {
-        if (!sortByRating && !sortByReleaseDate) {
-            // TODO: revert sort
-        } else {
-            // mutation?
-            this.getItems()
-                .sort((a, b) => (sortByReleaseDate ? new Date(b.date) - new Date(a.date) : 0))
-                .sort((a, b) => (sortByRating ? b.rating - a.rating : 0));
-            this.setState(this.state);
-        }
+    getSortedItems = () => {
+        const newItems = this.getItems().slice(0);
+        newItems
+            .sort((a, b) => (this.state.sortByReleaseDate ?
+                new Date(b.date) - new Date(a.date) : 0))
+            .sort((a, b) => (this.state.sortByRating ? b.rating - a.rating : 0));
+        return newItems;
     };
 
+    updateFilters = ({ sortByRating = false, sortByReleaseDate = false }) => {
+        this.setState({
+            sortByRating,
+            sortByReleaseDate,
+        });
+    }
     render() {
         if (!this.props.items || this.props.items.length === 0) {
             return (
@@ -58,12 +56,15 @@ class SearchResult extends Component {
                     <ItemsStatus items={this.getItems()} />
                     <span style={{ float: 'right' }}>
                         <ItemsSort
-                            sortOrder={{ sortByRating: true, sortByReleaseDate: false }}
-                            changeSortOrder={this.changeItemsOrder}
+                            sortOrder={{
+                                sortByRating: this.state.sortByRating,
+                                sortByReleaseDate: this.state.sortByReleaseDate,
+                            }}
+                            changeSortOrder={this.updateFilters}
                         />
                     </span>
                 </div>
-                <Films items={this.getItems()} onItemClick={this.state.onItemClick} />
+                <Films items={this.getSortedItems()} onItemClick={this.props.onItemClick} />
             </Fragment>
         );
     }
