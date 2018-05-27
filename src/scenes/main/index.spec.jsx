6837ea5aef6630f353be77912735bd45ee598369
Andrey Misaylov <misailovaa@gmail.com>
2018-05-27 12:43:15 +0300

    clear spec file for main

--- src/scenes/main/index.spec.jsx
+++ src/scenes/main/index.spec.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 
 import { Main } from './index';
 import { FilmInfo } from './film-Info';
-import { SearchBar } from './search-bar';
-
-
-// jest.mock('./search-result/empty-result.jsx', () => ({ EmptyResult: () => 'Nothing' }));
 
 describe('Main', () => {
     it('should show filmInfo for film view state', () => {
@@ -18,19 +13,5 @@ describe('Main', () => {
         });
 
         expect(main.find(FilmInfo).length).toBe(1);
-    });
-
-    it('just use enzyme', () => {
-        const main = shallow(<Main />);
-        console.log(main.find('#search-bar').debug());
-        expect(main.contains('<div>FIND YOUR MOVIE</div >')).toBe(false);
-
-        const wrapper = shallow((
-            <div>
-                <div data-foo="foo" data-bar="bar">Hello</div>
-            </div>
-        ));
-
-        expect(wrapper.contains(<div data-foo="foo" data-bar="bar">Hello</div>)).toBe(true);
     });
 });
