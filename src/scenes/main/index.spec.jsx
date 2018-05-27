1d15838e68a8dfdeb3f1e69f51b26b595c1e4ae0
Andrey Misaylov <misailovaa@gmail.com>
2018-05-27 12:41:02 +0300

    increase code coverage for main

--- src/scenes/main/index.spec.jsx
+++ src/scenes/main/index.spec.jsx
@@ -1,5 +1,36 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
 import { Main } from './index';
+import { FilmInfo } from './film-Info';
+import { SearchBar } from './search-bar';
+
+
+// jest.mock('./search-result/empty-result.jsx', () => ({ EmptyResult: () => 'Nothing' }));
 
 describe('Main', () => {
-    it('empty', () => { });
+    it('should show filmInfo for film view state', () => {
+        const main = shallow(<Main />);
+        main.setState({
+            view: 'film',
+            selectedFilm: { genre: 'action' },
+        });
+
+        expect(main.find(FilmInfo).length).toBe(1);
+    });
+
+    it('just use enzyme', () => {
+        const main = shallow(<Main />);
+        console.log(main.find('#search-bar').debug());
+        expect(main.contains('<div>FIND YOUR MOVIE</div >')).toBe(false);
+
+        const wrapper = shallow((
+            <div>
+                <div data-foo="foo" data-bar="bar">Hello</div>
+            </div>
+        ));
+
+        expect(wrapper.contains(<div data-foo="foo" data-bar="bar">Hello</div>)).toBe(true);
+    });
 });
