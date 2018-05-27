e89680468bcb9d049fbc8f7602fafe0888d4b464
Andrey Misaylov <misailovaa@gmail.com>
2018-05-27 12:07:26 +0300

    add app snapshot test

--- src/app.spec.js
+++ src/app.spec.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
 import { App } from './app';
 
 describe('App', () => {
     it('renders without crashing', () => {
-
+        const component = renderer.create(<App />).toJSON();
+        expect(component).toMatchSnapshot();
     });
 });
